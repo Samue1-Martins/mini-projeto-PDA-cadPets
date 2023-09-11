14fb62c690c70f14995bc6986b8ce2802c4def61
Samuel Douglas Martins Marques <samuel30douglas@gmail.com>
2023-09-11 11:55:36 -0300

    Atualizações

--- src/script/javaScript.js
+++ src/script/javaScript.js
@@ -26,7 +26,8 @@ const createClient = (client) => {
 }
 
 // Interaçao com o layout
-const createRow = (client) => {
+
+const createRow = (client, index) => {
     const newRow = document.createElement('tr')
     newRow.innerHTML = `
         <td>${client.nome}</td>
@@ -35,35 +36,51 @@ const createRow = (client) => {
         <td>${client.idade}</td>
         <td>${client.sexo}</td>
         <td>
-            <button>Editar</button>
-            <button>Excluir</button>
+            <button type ="button" id="edit-${index}">Editar Pet</button>
+            <button type ="button"id="delete-${index}">Excluir Pet</button>
         </td>
-        ` 
+    ` 
     document.querySelector('#tableClient>tbody').appendChild(newRow)
 }
+
+const clearTable = () =>{
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
 const updateTable = () =>{
     const dbClient = readClinet()
+    clearTable()
     dbClient.forEach(createRow)
 }
 
+updateTable()
 updateClient()
 
-// Events
 const isValueFields = () => {
     return document.getElementById('form').reportValidity()
 }
 
-
+// Botões editar excluir
+const editDelete = (event) =>{
+    if (event.target.type == 'button'){
+        const [action, index] = event.target.id.split('-')
+        console.log(action, index)
+        if (action == 'edit'){
+            console.log ('Editando Pet')
+        }else{
+            console.log('Excluindo Pet')
+        }
+    }
+}
 
 // Limpar campos input
-
 const clearFields = () => {
     const fields = document.querySelectorAll('.modal-field')
     fields.forEach(field => field.value = "")
 }
 
 // Botao salvar
-
 const saveClient = () => {
     if (isValueFields()){
         const client = {
@@ -74,11 +91,17 @@ const saveClient = () => {
             sexo: document.getElementById('sexo').value
         }
         createClient(client)
+        updateTable()
         clearFields()
         console.log("cadastrando cliente")
     }
 }
+// Events
 
 document.getElementById('salvar')
     .addEventListener('click', saveClient)
+    
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
+
 
